44834095707775e67794c6599f9126852f6cc010
ZavenPetrosyan <petrosyan_zaven@list.ru>
2018-09-09 10:35:24 +0400

    add node_fetch &  xml parse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
 
 const app = express();
 app.use("/graphql", graphqlHTTP({
+    schema,
     graphiql: true
 }));
-app.listen(4000, () => console.log(`Started at http://localhost:5000/graphql`));
+
+app.listen(4000, () => console.log(`Started at http://localhost:4000/graphql`));
