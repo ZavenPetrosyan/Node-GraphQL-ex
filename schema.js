f9ff5d315a5ea0c1599b838f0132f798b93290dc
ZavenPetrosyan <petrosyan_zaven@list.ru>
2018-09-22 19:19:59 +0400

    add dotenv

--- schema.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("node-fetch");
-const { promisify } = require("util");
-const xmlParser = promisify(require("xml2js"));
-
-fetch("https://www.goodreads.com/author/show/12855.Neil_deGrasse_Tyson")
-        .then(response => response.json())
-        console.log(response);
-
-module.exports = new GraphQLSchema()        
